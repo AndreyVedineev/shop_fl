e131348e7a5fe1689d482da78147ace270a0cf0b
avedineev <avedineev69@gmail.com>
2024-03-24 19:42:15 +0500

    After adding fixtures for goods

--- main/views.py
+++ main/views.py
@@ -1,13 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from goods.models import Categories
+
 # Create your views here.
 
 
 def index(request):
+
+    categories = Categories.objects.all()
+
+
+
     context = {
         "title": "Главная",
         "content": "Магазин цветов Букетная №1",
+        "categories": categories,
     }
     return render(request, "main/index.html", context)
 
