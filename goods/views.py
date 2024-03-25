d8b288c12751f8231094d9b873f1213f52eef12a
avedineev <avedineev69@gmail.com>
2024-03-25 21:44:20 +0500

    slug converter for procuct

--- goods/views.py
+++ goods/views.py
@@ -6,18 +6,15 @@ from goods.models import Products
 def catalog(request):
     goods = Products.objects.all()
 
+    context = {"title": "Букетная №1 - Каталог", "goods": goods}
+    return render(request, "goods/catalog.html", context)
 
 
-    context = {
-        'title': 'Букетная №1 - Каталог',
-        'goods':goods
+def product(request, product_slug=False, product_id=False):
+    if product_id:
+        product = Products.objects.get(id=product_id)
+    else:
+        product = Products.objects.get(slug=product_slug)
 
-    }
-    return render(request, 'goods/catalog.html', context)
-
-
-def product(request):
-    context = {
-
-    }
-    return render(request, 'goods/product.html', context)
+    context = {"product": product}
+    return render(request, "goods/product.html", context=context)
